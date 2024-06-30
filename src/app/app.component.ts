44c7c761dd7d9d61cf67f8d94757d5182347591b
Dale Andrew Adrayan <adrayandaleandrew@gmail.com>
2024-06-30 19:25:48 +0300

    some changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,7 +6,7 @@ import { RouterOutlet } from '@angular/router';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, CommonModule, MatIconModule  ],
+  imports: [RouterOutlet, CommonModule, MatIconModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
