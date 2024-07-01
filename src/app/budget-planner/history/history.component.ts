a6af49388ef28a28d96a30086243c793bdcb3284
Dale Andrew Adrayan <adrayandaleandrew@gmail.com>
2024-07-01 07:46:57 +0300

    added major changes on todo, profile & history directories

--- src/app/budget-planner/history/history.component.ts
+++ src/app/budget-planner/history/history.component.ts
@@ -3,17 +3,18 @@ import { Component } from '@angular/core';
 import { ReactiveFormsModule, FormBuilder, Validators } from '@angular/forms';
 import { MatIconModule } from '@angular/material/icon';
 import { Router } from '@angular/router';
+import { SideNavComponent } from '../side-nav/side-nav.component';
 
 @Component({
-  selector: 'app-todo',
+  selector: 'app-history',
   standalone: true,
-  imports: [ReactiveFormsModule, CommonModule, MatIconModule],
-  templateUrl: './todo.component.html',
-  styleUrl: './todo.component.scss'
+  imports: [ReactiveFormsModule,CommonModule,MatIconModule,SideNavComponent],
+  templateUrl: './history.component.html',
+  styleUrl: './history.component.scss'
 })
-export class TodoComponent {
+export class HistoryComponent {
   todoForm: any;
-  selectedMonth: any;
+  selectedMonth: string;
   expenses: { month: string, expenseAmount: number }[] = [
     { month: 'January', expenseAmount: 1500 },
     { month: 'February', expenseAmount: 2000 },
@@ -24,20 +25,17 @@ export class TodoComponent {
     { expenseType: 'Recharge', expenseAmount: 1000 },
     { expenseType: 'Light Bills', expenseAmount: 500 },
   ];
-
   februaryExpense: any[] = [
     { expenseType: 'Essentials', expenseAmount: 200 },
     { expenseType: 'Light Bills', expenseAmount: 400 }
   ];
-
   marchExpense: any[] = [
     { expenseType: 'Recharge', expenseAmount: 1100 },
     { expenseType: 'Essentials', expenseAmount: 250 }
   ];
+
   constructor(private fb: FormBuilder, private router: Router) {
-    const currentDate = new Date();
-    const currentMonth = currentDate.toLocaleString('default', { month: 'long' });
-    this.selectedMonth = currentMonth;
+    this.selectedMonth = new Date().toLocaleString('default', { month: 'long' });
   }
 
   ngOnInit(): void {
@@ -51,21 +49,8 @@ export class TodoComponent {
   onSubmitExpense() {
     if (this.todoForm.valid) {
       const newExpense = this.todoForm.value;
-      switch (this.selectedMonth) {
-        case 'January':
-          this.januaryExpense.push(newExpense);
-          break;
-        case 'February':
-          this.februaryExpense.push(newExpense);
-          break;
-        case 'March':
-          this.marchExpense.push(newExpense);
-          break;
-        default:
-          break;
-      }
+      this.getFilteredExpenses().push(newExpense);
       this.todoForm.reset();
-      this.todoForm.patchValue({ month: '', expenseType: '', expenseAmount: '' });
     }
   }
 
@@ -76,33 +61,7 @@ export class TodoComponent {
   }
 
   getFilteredExpenses() {
-    let filteredExpense: any[] = [];
     switch (this.selectedMonth) {
-      case 'January':
-        filteredExpense = [...this.januaryExpense];
-        break;
-      case 'February':
-        filteredExpense = [...this.februaryExpense];
-        break;
-      case 'March':
-        filteredExpense = [...this.marchExpense];
-        break;
-      default:
-        break;
-    }
-    return filteredExpense;
-  }
-
-  calculateTotalExpense(month: string): number {
-    let totalExpense = 0;
-    for (const income of this.gettodoFormonth(month)) {
-      totalExpense += income.expenseAmount;
-    }
-    return totalExpense;
-  }
-
-  gettodoFormonth(month: string): any[] {
-    switch (month) {
       case 'January':
         return this.januaryExpense;
       case 'February':
@@ -114,9 +73,12 @@ export class TodoComponent {
     }
   }
 
+  calculateTotalExpense(month: string): number {
+    return this.getFilteredExpenses().reduce((acc, curr) => acc + curr.expenseAmount, 0);
+  }
+
   onSave() {
     if (this.todoForm.valid) {
-      const incomeData = this.todoForm.value;
       this.todoForm.reset({ month: this.selectedMonth });
       this.getFilteredExpenses();
     }
@@ -128,9 +90,5 @@ export class TodoComponent {
 
   onBack() {
     this.router.navigate(['/budget-planner/dashboard']);
-  }
-
-  toggleSelection(expense: any) {
-    expense.selected = !expense.selected;
   }
 }
