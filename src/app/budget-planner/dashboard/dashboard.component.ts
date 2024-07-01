ca6ffdebb3cc0e7f2827157bed0cade03f8d099d
Dale Andrew Adrayan <adrayandaleandrew@gmail.com>
2024-07-01 04:58:23 +0300

    minor additions to the dashboard & added income dir and path

--- src/app/budget-planner/dashboard/dashboard.component.ts
+++ src/app/budget-planner/dashboard/dashboard.component.ts
@@ -1,12 +1,52 @@
 import { Component } from '@angular/core';
+import { MatIconModule } from '@angular/material/icon';
+import { SideNavComponent } from '../side-nav/side-nav.component';
+import { CommonModule } from '@angular/common';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-dashboard',
   standalone: true,
-  imports: [],
+  imports: [MatIconModule, SideNavComponent, CommonModule],
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.scss'
 })
 export class DashboardComponent {
+  // Data for Income
+  lastMonthsIncome = ['January: $1000', 'February: $1500', 'March: $1200'];
+  currentMonthsIncome = '$2000';
 
+  // Expense
+  lastMonthsExpense = ['January: $800', 'February: $1000', 'March: $1200'];
+  currentMonthsExpense = '$1500';
+
+  // Todo Transaction
+  todoTransactions = [
+    {description: 'Pay electricity bill'},
+    {description: 'Submit monthly report'},
+    {description: 'Buy groceries'},
+    {description: 'Call insurance company'}
+  ];
+
+  // TOTAL
+  totalCurrentMonthIncome = 2000;
+  totalCurrentMonthExpense = 2000;
+  
+  constructor(public router:Router){ }
+
+  onIncome(){
+    this.router.navigate(['./budget-planner/income']);
+  }
+
+  onExpense(){
+    this.router.navigate(['./budget-planner/expense']);
+  }
+
+  onTodo(){
+    this.router.navigate(['./budget-planner/tood']);
+  }
+
+  get currentMonthSavings(): number {
+    return this.totalCurrentMonthIncome - this.totalCurrentMonthExpense;
+  }
 }
